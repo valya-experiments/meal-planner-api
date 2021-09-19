33f56f6a066da47510249553856f467453d8f935
Valentin Berlin <valentin.berlin@protonmail.com>
2021-09-19 13:15:16 +0200

    remove unused database scripts

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -4,6 +4,7 @@ import { getMeals } from "../controllers/meals/getMeals";
 import { patchMeals } from "../controllers/meals/patchMeals";
 import { postMeals } from "../controllers/meals/postMeals";
 import { DatabaseAPI } from "../services/database";
+
 const router = express.Router();
 
 export function makeMealsRouter(db: DatabaseAPI): Router {
